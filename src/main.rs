560d7f4f1d00b42732624560452cbf51576e2a11
nicholas-roether <nicholas.roether@t-online.de>
2023-03-18 17:32:12 +0100

    Load .env in debug/test mode

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use config::{
 use server::Server;
 use tokio::io::AsyncWriteExt;
 
+#[cfg(any(debug_assertions, test))]
+use dotenvy::dotenv;
+
 mod server;
 
 mod config;
@@ -17,12 +20,13 @@ mod utils;
 
 #[tokio::main]
 async fn main() {
+	#[cfg(any(debug_assertions, test))]
+	dotenv().ok();
+
 	logger::init();
 
 	let Parameters { port } = params::read();
-
 	let tls_params = fatal_error!(read_tls_params().await);
-
 	let server = fatal_error!(Server::bind(port, tls_params).await);
 
 	server
