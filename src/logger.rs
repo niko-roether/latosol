2abd00734eb8eec1d4fdad3c605a4da1dee25c5b
nicholas-roether <nicholas.roether@t-online.de>
2023-03-18 05:47:37 +0100

    Fix env_logger config

--- src/logger.rs
+++ src/logger.rs
@@ -2,8 +2,7 @@ use env_logger::Env;
 
 pub fn init() {
 	let env = Env::default()
-		.filter("LATOSOL_LOG")
-		.write_style("LATOSOL_LOG_STYLE")
-		.default_filter_or("info");
+		.filter_or("LATOSOL_LOG", "info")
+		.write_style("LATOSOL_LOG_STYLE");
 	env_logger::init_from_env(env);
 }
